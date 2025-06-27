eb8d0f813ab69311da90528adf45372d08331136
Pramodkumar1306 <pramod73kumar7415@gmail.com>
2025-06-27 21:54:41 +0530

    added12asd

--- frontEnd/vite.config.js
+++ frontEnd/vite.config.js
@@ -1,14 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-// import tailwindcss from '@tailwindcss/vite'
-// /** @type {import('tailwindcss').Config} */
-// https://vite.dev/config/
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// // import tailwindcss from '@tailwindcss/vite'
+// // /** @type {import('tailwindcss').Config} */
+// // https://vite.dev/config/
+// export default defineConfig({
+//   content: [
+//     "./src/**/*.{js,jsx,ts,tsx}", // 👈 this line includes all your React files
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [react() ],
+// })
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // 👈 this line includes all your React files
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [react(),tailwindcss()],
-})
+  plugins: [react()]
+});
