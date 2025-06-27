8b8049480e3000ac4d2b2d9870b63d4138bcab01
Pramodkumar1306 <pramod73kumar7415@gmail.com>
2025-06-27 21:28:23 +0530

    added1

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -28,5 +28,5 @@ connectDb();
   app.use('/api/auth', authRoutes);
   app.use('/api/auth',authRoutes)
   // Start server
-  const PORT = process.env.PORT;
+  const PORT = process.env.PORT || 4000;
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
